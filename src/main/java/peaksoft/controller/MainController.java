642693b8bb79df9ad3a6c4ab3e9fe61373264918
Mukhammed Dev <muhammedtoichubai@gmail.com>
2024-11-07 20:02:46 +0600

    start config for mvc

--- src/main/java/peaksoft/controller/MainController.java
+++ src/main/java/peaksoft/controller/MainController.java
@@ -1,4 +1,21 @@
 package peaksoft.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/main")
 public class MainController {
+
+
+    @GetMapping
+    public String mainPage(){
+        return "main-page";
+    }
+
+    @GetMapping("/greetings")
+    public String indexPage(){
+        return "greetings";
+    }
 }
